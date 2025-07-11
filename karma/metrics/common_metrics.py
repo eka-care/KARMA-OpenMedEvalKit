6b6eccdba5ff916a1b4d7155fc2f8d5fcd7c74c4
ViswaVasanth-2002 <vasanth.murukuri@ekasupport.com>
2025-07-11 11:06:05 +0530

    add tokenised f1 to metrics

--- karma/metrics/common_metrics.py
+++ karma/metrics/common_metrics.py
@@ -1,5 +1,7 @@
 import evaluate
-
+from sklearn.metrics import f1_score
+from collections import Counter
+import re
 from karma.metrics.base_metric_abs import BaseMetric
 from karma.registries.metrics_registry import register_metric
 
@@ -55,3 +57,36 @@ class WERMetric(HfMetric):
 class CERMetric(HfMetric):
     def __init__(self, metric_name: str = "cer", **kwargs):
         super().__init__(metric_name)
+
+@register_metric("tokenised_f1")
+class TokenisedF1Metric(BaseMetric):
+    def __init__(self, metric_name: str = "tokenised_f1", **kwargs):
+        super().__init__(metric_name)
+
+    def tokenize(self, text):
+        return re.findall(r'\w+', text.lower())
+    
+    def evaluate(self, predictions, references , **kwargs):
+        f1_scores = []
+        for prediction, reference in zip(predictions, references):
+            pred_tokens = self.tokenize(prediction)
+            gold_tokens = self.tokenize(reference)
+    
+            pred_counts = Counter(pred_tokens)
+            gold_counts = Counter(gold_tokens)
+    
+            # Compute overlap
+            common = pred_counts & gold_counts
+            num_same = sum(common.values())
+    
+            if num_same == 0:
+                f1_scores.append(0.0)
+                continue
+    
+            precision = num_same / len(pred_tokens)
+            recall = num_same / len(gold_tokens)
+            f1 = 2 * precision * recall / (precision + recall)
+            f1_scores.append(f1)
+        return sum(f1_scores)/len(f1_scores)
+
+    
