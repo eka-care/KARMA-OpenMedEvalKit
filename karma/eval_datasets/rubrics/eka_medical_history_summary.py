6da0d840a38919826cedfb98d9dbe6f4bae083f3
Nikhil Kasukurthi <nikhil.kasukurthi@eka.care>
2025-07-17 15:28:32 +0530

    Add refresh cache

--- karma/eval_datasets/rubrics/eka_medical_history_summary.py
+++ karma/eval_datasets/rubrics/eka_medical_history_summary.py
@@ -1,13 +1,22 @@
+from datetime import datetime
 import os
 
 from karma.eval_datasets.rubrics.rubric_base_dataset import RubricBaseDataset
 from karma.registries.dataset_registry import register_dataset
 
+# base_default_system_prompt = f"""
+# You are a medical copilot with expansive medical knowledge that should mimic an experienced medical professional (a doctor).
+# You will never paraphrase or speculate information, always faithfully reproduce the evidence as reported in all the output.
+# Always cross-reference information across different sections of the patient data to ensure consistency in reporting
+# Today is {datetime.now().strftime("%Y-%m-%d")}. Compute all relative dates in comparison to today.
+# All dates of the evidences are formatted as %Y-%m-%d.
+# Generate a succinct summary of the medical history for the most recent 6 months for a doctor.
+# """
 base_default_system_prompt = """You are a helpful assistant. Generate a succinct summary of the medical history for the last 6 months."""
 
 DATASET_NAME = "ekacare/ekacare_medical_history_summarisation"
 SPLIT = "test"
-COMMIT_HASH = "9098354aaa37633264117f74212d6b80983d0a21"
+COMMIT_HASH = "ef267abbf76bd032aaaf40053ad67e1e44488ada"
 
 
 @register_dataset(
@@ -20,11 +29,10 @@ COMMIT_HASH = "9098354aaa37633264117f74212d6b80983d0a21"
 )
 class EkaMedicalHistorySummary(RubricBaseDataset):
     def __init__(self, system_prompt=base_default_system_prompt, **kwargs):
-        from huggingface_hub import login
-
-        try:
-            login(os.getenv("HUGGINGFACE_TOKEN"))
-        except ValueError as e:
-            raise e
-        super().__init__(system_prompt=system_prompt, **kwargs)
+        super().__init__(
+            dataset_name=DATASET_NAME,
+            split=SPLIT,
+            system_prompt=system_prompt,
+            **kwargs,
+        )
         self.system_prompt = system_prompt
